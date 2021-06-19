a7de8f100b697ce3ee2e40abf3c93948c7bbeec3
Jones Ogolo <47540149+Jay-Topher@users.noreply.github.com>
2021-06-19 01:57:45 +0100

    projects

---
+++ constants/projects.js
@@ -0,0 +1,31 @@
+const projects = [
+  {
+    title: "Banque",
+    description:
+      "Banque is a savings app that helps all consumers save and transfer money seamlessly and securely without charges while at the comfort of their homes.",
+    technologies: ["React", "Redux", "NodeJs", "ExpressJs", "MongoDB", "SCSS"],
+    imgSrc: "/banque.jpeg",
+    direction: "ltr",
+    link: "https://banque-end.herokuapp.com/",
+  },
+  {
+    title: "Brand Focus feed Mill",
+    description:
+      "Brand Focus feed Mill a Nigerian-Based feed mill factory that deals in production and distribution of quality poultry feeds with clientele spanning across states.",
+    technologies: ["React(NextJs)", "SCSS"],
+    imgSrc: "/brand-focus.jpeg",
+    direction: "rtl",
+    link: "https://www.riversfeeds.com/",
+  },
+  {
+    title: "Maramoja",
+    description:
+      "MARAMOJA is a location based taxi app that enables you and your friends to select your favorite drivers who will always be offered to you anytime they are nearby and you need to go.",
+    technologies: ["React"],
+    imgSrc: "/maramoja.jpeg",
+    direction: "ltr",
+    link: "https://maramoja.co.ke/",
+  },
+];
+
+export default projects;
