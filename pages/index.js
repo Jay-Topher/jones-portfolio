d612ffc9dbd9269de4a4464b5ffd644999761504
Jones Ogolo <47540149+Jay-Topher@users.noreply.github.com>
2021-06-20 23:52:58 +0100

    mount work history

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Projects from "../components/Projects";
+import WorkHistory from "../components/WorkHistory";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Hero />
       <About />
       <Projects />
+      <WorkHistory />
     </div>
   );
 }
