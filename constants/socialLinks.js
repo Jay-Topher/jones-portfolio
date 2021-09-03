bcd8bd78b74755217f25a52ad03b1d7c9c43e40b
Jones Ogolo <47540149+Jay-Topher@users.noreply.github.com>
2021-09-03 09:20:29 +0100

    add social inks

---
+++ constants/socialLinks.js
@@ -0,0 +1,26 @@
+import { GitHub, Linkedin, Mail, Twitter } from "react-feather";
+
+const socialLinks = [
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/jones-christopher/",
+    icon: Linkedin,
+  },
+  {
+    name: "Github",
+    link: "https://github.com/Jay-Topher",
+    icon: GitHub,
+  },
+  {
+    name: "Twitter",
+    link: "https://github.com/Jay-Topher",
+    icon: Twitter,
+  },
+  {
+    name: "Email",
+    link: "mailto:jones.ogolo@gmail.com",
+    icon: Mail,
+  },
+];
+
+export default socialLinks;
