65c7e61f5088186decd1a68107306b9471fb1b10
Jones Ogolo <47540149+Jay-Topher@users.noreply.github.com>
2021-09-07 00:20:55 +0100

    commot social i\link

--- components/Socials.js
+++ components/Socials.js
@@ -48,6 +48,12 @@ function Socials() {
         .link-item:hover {
           transform: scale(1.3);
         }
+
+        @media only screen and (max-width: 800px) {
+          .socials {
+            display: none;
+          }
+        }
       `}</style>
     </aside>
   );
