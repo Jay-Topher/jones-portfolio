0544f2dc53def865e8a6ea21625bbfb7af572f78
Jones Ogolo <47540149+Jay-Topher@users.noreply.github.com>
2021-09-07 00:42:11 +0100

    fix btn

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,9 @@
-import { Fragment } from "react";
 import Link from "next/link";
 import colors from "../constants/colors";
 
-function Button({ to, variant, className, isLink = false, ...props }) {
-  const Wrapper = isLink ? Fragment : Link;
-  return (
-    <Wrapper href={isLink ? null : to}>
+function Button({ to, variant, className, isLink, ...props }) {
+  return isLink ? (
+    <Link href={to}>
       <a
         className={`button ${variant} ${className}`}
         href={isLink ? to : null}
@@ -36,7 +34,34 @@ function Button({ to, variant, className, isLink = false, ...props }) {
           }
         `}</style>
       </a>
-    </Wrapper>
+    </Link>
+  ) : (
+    <a className={`button ${variant} ${className}`} href={to} {...props}>
+      {props.children}
+
+      <style jsx>{`
+        .filled {
+          color: #fff;
+          background-color: ${colors.primary};
+        }
+
+        .outlined {
+          border: 1px solid ${colors.primary};
+          color: ${colors.primary};
+          text-align: center;
+        }
+        .button {
+          padding: 5px 20px;
+          border-radius: 3px;
+          text-transform: uppercase;
+          cursor: pointer;
+        }
+
+        .hire-btn {
+          margin-right: 10px;
+        }
+      `}</style>
+    </a>
   );
 }
 
