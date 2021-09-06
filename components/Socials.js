5ef93c63d1545b828aee27ca44ec42b8c1f14297
Jones Ogolo <47540149+Jay-Topher@users.noreply.github.com>
2021-09-07 00:16:40 +0100

    restyle socials

--- components/Socials.js
+++ components/Socials.js
@@ -1,18 +1,52 @@
-import { GitHub, Linkedin, Mail, Twitter } from "react-feather";
 import socialLinks from "../constants/socialLinks";
 
 function Socials() {
   return (
     <aside className="socials">
-      <GitHub color="#fff" />
+      <div className="link-container">
+        {socialLinks.map((item) => (
+          <a
+            key={item.name}
+            href={item.link}
+            target="_blank"
+            rel="noreferrer"
+            className="link-item"
+          >
+            <item.icon color="#fff" />
+          </a>
+        ))}
+      </div>
 
       <style jsx>{`
         .socials {
-          background-color: #000000aa;
-          padding: 15px;
           position: fixed;
-          top: 50%;
-          right:0;
+          top: 0;
+          right: 0;
+          height: 100vh;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+
+        .link-container {
+          background-color: #000000aa;
+          border-radius: 10px 0 0 10px;
+          padding: 15px;
+          display: flex;
+          flex-direction: column;
+          /* justify-content: space-between; */
+        }
+
+        .link-item {
+          transition: all 0.3s ease-in;
+        }
+
+        .link-item:not(:last-child) {
+          margin-bottom: 10px;
+        }
+
+        .link-item:hover {
+          transform: scale(1.3);
         }
       `}</style>
     </aside>
