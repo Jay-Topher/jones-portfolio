c9828e3f4b3342808deaf2f503394d7d70e10fe1
Jones Ogolo <47540149+Jay-Topher@users.noreply.github.com>
2022-01-18 20:42:05 +0100

    add blurbkit to projects

--- constants/projects.js
+++ constants/projects.js
@@ -1,11 +1,20 @@
 const projects = [
   {
+    title: "Blurbkit",
+    description:
+      "At Blurbkit we believe that words and a voice is all we need to tap into the wealth of Africa’s uniqueness, so the world can enjoy more and appreciate even better Africa’s greatness.",
+    technologies: ["React(NextJs)"],
+    imgSrc: "/blurbkit.webp",
+    direction: "ltr",
+    link: "https://blurbkit.com/",
+  },
+  {
     title: "Maramoja",
     description:
       "MARAMOJA is a location based taxi app that enables you and your friends to select your favorite drivers who will always be offered to you anytime they are nearby and you need to go.",
     technologies: ["React", "NodeJS"],
     imgSrc: "/maramoja.jpeg",
-    direction: "ltr",
+    direction: "rtl",
     link: "https://maramoja.co.ke/",
   },
   {
@@ -14,7 +23,7 @@ const projects = [
       "Brand Focus feed Mill a Nigerian-Based feed mill factory that deals in production and distribution of quality poultry feeds with clientele spanning across states.",
     technologies: ["React(NextJs)", "SCSS"],
     imgSrc: "/brand-focus.jpeg",
-    direction: "rtl",
+    direction: "ltr",
     link: "https://www.riversfeeds.com/",
   },
   {
@@ -23,10 +32,9 @@ const projects = [
       "Banque is a savings app that helps all consumers save and transfer money seamlessly and securely without charges while at the comfort of their homes.",
     technologies: ["React", "Redux", "NodeJs", "ExpressJs", "MongoDB", "SCSS"],
     imgSrc: "/banque.jpeg",
-    direction: "ltr",
+    direction: "rtl",
     link: "https://banque-end.herokuapp.com/",
   },
-  
 ];
 
 export default projects;
