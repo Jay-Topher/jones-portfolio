f5155d7c2e350d03a7c09a322e16e0baa76e6969
Jones Ogolo <47540149+Jay-Topher@users.noreply.github.com>
2022-07-28 12:13:57 +0100

    change cv url

--- constants/socialLinks.js
+++ constants/socialLinks.js
@@ -23,7 +23,7 @@ const socialLinks = [
   },
   {
     name: "CV",
-    link: "https://drive.google.com/file/d/1-cuDVLjtNDovQDHPdtmawMoG0N_a23ty/view?usp=sharing",
+    link: "https://drive.google.com/file/d/13otDL6crlM-QtnVljp8wBWfU7mnY3sso/view?usp=sharing",
     icon: FileText,
   },
 ];
